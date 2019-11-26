e79898957c153dbf6b02e2bf4c1ffd1c7f542978
MicroPower - Victor Bezerra Santochi Richtmann <vrichtmann@gmail.com>
2019-11-26 15:37:37 -0300

    Movimentação personagem

--- Assets/PlayerMoviment.cs
+++ Assets/PlayerMoviment.cs
@@ -4,33 +4,48 @@ using UnityEngine;
 
 public class PlayerMoviment : MonoBehaviour
 {
+    [Header("Input settigs:")]
     public float moveSpeed = 0.5f;
 
+    [Space]
+    [Header("Character Atributes")]
+    public float playerSpeed = 20;
+
+    [Space]
+    [Header("Character Statistics:")]
+    Vector2 movimentDir;
+
+    [Space]
+    [Header("References")]
     public Rigidbody2D rb;
     public Animator animator;
-
-    Vector2 moviment;
+    public SpriteRenderer spriteRD;
 
     void Update(){
-       
-        moviment.x = Input.GetAxis("Horizontal");
-        moviment.y = Input.GetAxis("Vertical");
+        ProcessInputs();
+        Move();
+        Animate();
     }
 
-    void FixedUpdate() {
 
-        Debug.Log("moviment.x " + moviment.x);
-        //Debug.Log("moviment.y " + moviment.y);
-        //Debug.Log("moviment.sqrMagnitude : " + moviment.sqrMagnitude);
-        this.transform.position = new Vector3(this.transform.position.x + (moviment.x * moveSpeed), this.transform.position.y + (moviment.y * moveSpeed), this.transform.position.z);
+    void ProcessInputs()
+    {
+        movimentDir = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        moveSpeed = Mathf.Clamp(movimentDir.magnitude, 0.0f, 1.0f);
+        movimentDir.Normalize();
+    }
 
-      
-        this.GetComponent<SpriteRenderer>().flipX = (moviment.x < 0);
+    void Move(){
+        rb.velocity = movimentDir * moveSpeed * playerSpeed;
+    }
+
+    void Animate(){
+        if(movimentDir != Vector2.zero){
+            spriteRD.flipX = (movimentDir.x < 0);
+            animator.SetFloat("Horizontal", movimentDir.x);
+            animator.SetFloat("Vertical", movimentDir.y);
+        }
        
-        animator.SetFloat("Horizontal", moviment.x);
-        animator.SetFloat("Vertical", moviment.y);
-        animator.SetFloat("Speed", moviment.sqrMagnitude);
-        //rb.AddForce(new Vector2(moviment.x, moviment.y));
-        //rb.MovePosition(rb.position * moviment * moveSpeed * Time.fixedDeltaTime);
+        animator.SetFloat("Speed", moveSpeed);
     }
 }
