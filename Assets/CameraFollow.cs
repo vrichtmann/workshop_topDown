5d4f4f33848d3e234301afc781d1de58ce1a3fa3
MicroPower - Victor Bezerra Santochi Richtmann <vrichtmann@gmail.com>
2019-11-28 16:06:17 -0300

    Implementei Movimetção basica e movimentação de camera 👉👈👇☝️🌁

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    private Func<Vector3> GetCameraFollowPositionFunc;
+
+   public void Setup(Func<Vector3> GetCameraFollowPositionFunc){
+        this.GetCameraFollowPositionFunc = GetCameraFollowPositionFunc;
+   }
+
+    public void SetGetCameraFollowPositionFunc(Func<Vector3> GetCameraFollowPositionFunc)
+    {
+        this.GetCameraFollowPositionFunc = GetCameraFollowPositionFunc;
+    }
+
+   void Update(){
+        Vector3 cameraFollowPosition = GetCameraFollowPositionFunc();
+        cameraFollowPosition.z = transform.position.z;
+
+        Vector3 cameraMoveDir = (cameraFollowPosition - transform.position).normalized;
+        float distance = Vector3.Distance(cameraFollowPosition, transform.position);
+        float cameraMoveSpeed = 2f;
+
+        Debug.Log("cameraMoveDir : " + cameraMoveDir);
+        Debug.Log("distance : " + distance);
+        Debug.Log("Time.deltaTime : " + Time.deltaTime);
+
+        if(distance > 0)
+        {
+            Vector3 newCameraPosition = transform.position + cameraMoveDir * distance * cameraMoveSpeed * Time.deltaTime;
+
+            float distanceAfterMoving = Vector3.Distance(newCameraPosition, cameraFollowPosition);
+
+            if(distanceAfterMoving > distance){
+                newCameraPosition = cameraFollowPosition;
+            }
+
+            transform.position = newCameraPosition;
+        }
+
+        
+        //transform.position = cameraFollowPosition;
+    }
+}
---
+++ Assets/GameHandler.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.UI;
+
+public class GameHandler : MonoBehaviour
+{
+   
+    public CameraFollow cameraFollow;
+    public Transform playerTransform;
+    public Transform character1Transform;
+    public Transform character2Transform;
+    public Transform ManualMovimentTransform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cameraFollow.Setup(() => playerTransform.position);
+    }
+
+    public void selectCameraChar(int _camera){
+        if (_camera == 1){
+            cameraFollow.SetGetCameraFollowPositionFunc(() => playerTransform.position);
+        }
+        else if (_camera == 2){
+            cameraFollow.SetGetCameraFollowPositionFunc(() => character1Transform.position);
+        }
+        else if (_camera == 3){
+            cameraFollow.SetGetCameraFollowPositionFunc(() => character2Transform.position);
+        }
+        else if (_camera == 4) {
+            cameraFollow.SetGetCameraFollowPositionFunc(() => ManualMovimentTransform.position);
+        }
+    }
+
+}
